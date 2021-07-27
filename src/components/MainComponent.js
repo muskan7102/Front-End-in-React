8143868208446a72b8c153537ac3dedbd03f9774
Muskan Goyal <muskangoyal7102@gmail.com>
2021-07-27 09:58:54 +0530

    Assignment-2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,38 +1,93 @@
-import React,{ Component } from 'react';
-import { Navbar,NavbarBrand } from 'reactstrap';
+import React, { Component } from 'react';
+
+import Home from './HomeComponent';
 import Menu from './menu_component';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
+
+import { COMMENTS } from '../shared/comments'
+import { DISHES } from '../shared/dishes'
+import { LEADERS } from '../shared/leaders'
+import { PROMOTIONS } from '../shared/promotions'
+
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
-  constructor(props){
-    super(props);
 
-    this.state={ dishes: DISHES,
-    selectedDish: null};
-  }
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-}
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            comments: COMMENTS,
+            dishes: DISHES,
+            leaders: LEADERS,
+            promotions: PROMOTIONS,
+        };
+
+    }
+
+    render() {
+
+        const HomePage = () => {
+            return(
+                <Home 
+                    dish={ this.state.dishes.filter( (dish)=>dish.featured )[0] }
+                    promotion={this.state.promotions.filter( (promotion)=>promotion.featured )[0] }
+                    leader={this.state.leaders.filter( (leader)=>leader.featured )[0] }
+                />
+            );
+        };
+
+        const AboutUsPage = () => {
+            return(
+                <About 
+                    leaders={this.state.leaders}
+                />
+            );
+        };
 
 
-  render(){
-    return (
-      <div >
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">
-              Ristorante con fusion
-            </NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes = {this.state.dishes} onClick = {(dishId) => this.onDishSelect( dishId )}></Menu> 
-        <div className="container">
-        <DishDetail dish ={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]}></DishDetail> 
-        </div>
-      </div>
-    );
-  }
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail 
+                
+                dish={this.state.dishes.filter( (dish) => dish.id === parseInt(match.params.dishId, 10))[0] } 
+                comments={this.state.comments.filter( (comment) => comment.dishId === parseInt(match.params.dishId, 10)) } 
+                
+                
+                />
+            );
+        };
+
+
+
+        return (
+            <div>
+                <Header></Header>
+
+                <Switch>
+                    <Route path="/home" component={ HomePage } />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/> }/>
+
+                    <Route path="/menu/:dishId" component={DishWithId} />
+
+                    <Route exact path="/contactus" component={Contact } />
+                    <Route exact path="/aboutus" component={ AboutUsPage } />
+                   
+                    {/* if url dosesnt match, bydefault redirect to */}
+                    <Redirect to="/home" />
+                </Switch>
+
+                <Footer></Footer>
+            </div> 
+        );
+
+    }
+
 }
 
 export default Main;
--- src/components/menu_component.js
+++ src/components/menu_component.js
@@ -1,28 +1,27 @@
-import React ,{ Component } from 'react';
-import { Media } from 'reactstrap';
-import DishDetail from './DishdetailComponent';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'reactstrap';
-class Menu extends Component{
-     constructor(props){
-         super(props);
-         
-        console.log('Menu component constructed');
+import React from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem  } from "reactstrap";
+import {Link} from 'react-router-dom';
+     
+
+    function RenderMenuItem({ dish, onClick }) {
+        return(
+            <Card>
+                <Link to={ `/menu/${dish.id}` } >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle> {dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
+            </Card>   
+        );
     }
 
+    const Menu = (props) => {
 
-    render(){
-        console.log('renders menu component');
-        
-        const menu = this.props.dishes.map((dish) => {
+        const menu = props.dishes.map((dish) => {
             return (
                 <div key={ dish.id } className="col-12 col-md-5 m-1">    
-                    <Card onClick={() => this.props.onClick(dish.id) } >
-
-                        <CardImg width="100%" src={ dish.image } alt={ dish.name } />
-                        <CardImgOverlay>
-                            <CardTitle> { dish.name }</CardTitle>
-                        </CardImgOverlay>
-                    </Card>                
+                    <RenderMenuItem dish={dish}  />
                 </div>
             );
         });
@@ -30,15 +29,31 @@ class Menu extends Component{
         return(
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/home">Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            Menu
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                    
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
+
+                <div className="row">
                     { menu }
                 </div>
             </div>
         );
-    }
 
-    componentDidMount(){
-        console.log('Menu component componentDidMounbt is invoked');
-        
     }
-}
-export default Menu; 
+    
+
+
+
+
+export default Menu;
